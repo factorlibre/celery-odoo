6f73b526fd74b9dd1ef9c1c3acae7f3490227b47
Hugo Santos <hugo.santos@factorlibre.com>
2016-06-07 18:39:18 +0200

    [ADD] celery_queue
    - Module to execute Odoo task as background jobs in celery workers

---
+++ README.md
@@ -0,0 +1,6 @@
+
+Odoo Celery Task
+=============================
+
+
+This project is created to add modules realated with the execution of Odoo tasks in a celery queue
---
+++ celery_queue/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2016 FactorLibre - Hugo Santos <hugo.santos@factorlibre.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Celery Queue',
+    'version': '8.0.1.0.0',
+    'depends': [
+        'base'
+    ],
+    'author': 'Odoo Community Association (OCA),FactorLibre',
+    'license': 'AGPL-3',
+    'website': 'http://www.factorlibre.com',
+    'data': [],
+    'installable': True,
+    'application': False,
+    'external_dependencies': {
+        'python': ['celery'],
+    }
+}
